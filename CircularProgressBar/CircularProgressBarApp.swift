17cc316c08b450d230d3bebc9162c21fe6dd4d14
Elaidzha1940 <eli080293@gmail.com>
2023-09-24 04:21:15 +0300

    on 24.09.2023.

---
+++ CircularProgressBar/CircularProgressBarApp.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: CircularProgressBar
+//  File: CircularProgressBarApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 24.09.2023
+//
+//  */
+
+import SwiftUI
+
+@main
+struct CircularProgressBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CircularProgressBar/ContentView.swift
@@ -0,0 +1,23 @@
+//  /*
+//
+//  Project: CircularProgressBar
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 24.09.2023
+//
+//  */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+        
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
