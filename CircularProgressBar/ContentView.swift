8ca8565fbfb0ab8b800e33d4eb8683e908897f50
Elaidzha1940 <eli080293@gmail.com>
2023-09-25 01:17:06 +0300

    Cirlce()

--- CircularProgressBar/ContentView.swift
+++ CircularProgressBar/ContentView.swift
@@ -12,9 +12,26 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        VStack {
-        
+        ZStack {
+            Circle()
+                .stroke(lineWidth: 20)
+                .frame(width: 200, height: 200)
+                .foregroundColor(.white)
+                .shadow(color: .black.opacity(0.1), radius: 10, x: 10, y: 10)
+            
+            Circle()
+                .stroke(lineWidth: 20)
+                .frame(width: 1750, height: 175)
+                .foregroundStyle(LinearGradient(gradient: Gradient(colors: [Color.black, Color.clear]), startPoint: .bottomLeading, endPoint: .topLeading))
+                .overlay {
+                    Circle()
+                        .stroke(.black.opacity(0.1), lineWidth: 2)
+                        .blur(radius: 5)
+                }
+
+            
         }
+        .preferredColorScheme(.light)
     }
 }
 
